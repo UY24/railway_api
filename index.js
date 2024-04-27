df9cf69b4a85862d08d37c9baffa7cad723655fd
UY24 <ujjwal.uy03@gmail.com>
2024-04-27 12:08:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = 3000;
+const setupAndStartServer = () => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+  });
+};
+
+setupAndStartServer();
